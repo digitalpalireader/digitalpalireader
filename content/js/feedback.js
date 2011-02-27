0caf5aafe086120f6567d999fa51110284739267
Noah Yuttadhammo <yuttadhammo@users.sourceforge.net>
2011-02-27 17:57:15 +0530

    updated contact form to use iframe

--- content/js/feedback.js
+++ content/js/feedback.js
@@ -1,22 +1,13 @@
 function feedbackform() {
-	var mafbout = '<form action="http://pali.sirimangalo.org/dprmail.php" method="POST">'
-	+ '<p><font size=5><b>Feedback Form</b></font>'
-	+ '<p><em>This form allows you to send comments, questions or bug reports directly to the creator of the DPR.<br>'
-	+ 'Your email goes straight to me, so there is no need to worry about spam, etc.</em></p>'
-	+ '<p>'
-	+ '<b>Your Email</b>'
-	+ '<br>'
-	+ '<input type="text" name="email" size=40>'
-	+ '<p>'
-	+ '<b>Subject</b>'
-	+ '<br>'
-	+ '<input type="text" name="subject" size=40>'
-	+ '<p>'
-	+ '<b>Message</b>'
-	+ '<br>'
-	+ '<textarea cols=40 rows=10 name="message"></textarea>'
-	+ '<p><input type="submit" value=" Send ">'
-	+ '</form>';
-	document.getElementById('mafbc').innerHTML=mafbout;
+	moveframex(1);
+	moves();
+
+	var outNode = document.createElement('iframe');
+	outNode.setAttribute('frameBorder','0');
+	outNode.setAttribute('width','100%');
+	outNode.setAttribute('height',(document.getElementById('maf').offsetHeight-64)+'px');
+	outNode.setAttribute('src','contact.htm');
+	document.getElementById('mafbc').innerHTML='';
+	document.getElementById('mafbc').appendChild(outNode);
 }
 
