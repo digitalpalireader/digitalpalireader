ae382ae1a2d395a43dfa2535aebd688bbe61e461
Noah Yuttadhammo <yuttadhammo@users.sourceforge.net>
2011-04-13 19:55:59 +0530

    minor changes

--- content/js/navigation.js
+++ content/js/navigation.js
@@ -815,21 +815,3 @@ function importXMLindex(reuse) {
 		openDPRTab(permalink,'DPR index');
 	}
 }
-
-function openDPRTab(permalink,id) {
-	
-	var mainWindow = window.QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-                   .getInterface(Components.interfaces.nsIWebNavigation)
-                   .QueryInterface(Components.interfaces.nsIDocShellTreeItem)
-                   .rootTreeItem
-                   .QueryInterface(Components.interfaces.nsIInterfaceRequestor)
-                   .getInterface(Components.interfaces.nsIDOMWindow); 
-
-	var newTab = mainWindow.gBrowser.addTab(permalink);
-
-	if(id) newTab.setAttribute('id', id);
-	mainWindow.gBrowser.moveTabTo(newTab, mainWindow.gBrowser.selectedTab.tabIndex+1)
-	mainWindow.gBrowser.selectedTab = newTab;
-
-}
-
