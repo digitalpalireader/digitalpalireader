98d0854f05fd32907699039a12ba4d7684f6b829
Noah Yuttadhammo <yuttadhammo@users.sourceforge.net>
2011-04-20 08:42:04 +0530

    fixing css

--- content/js/config_sidebar.js
+++ content/js/config_sidebar.js
@@ -1,72 +1,24 @@
-var atiIns = 0;
-
-function changeStyleByName(name,attrib,value) {
-		var tags = document.getElementsByName(name);
-		for(var i=0;i < tags.length;i++)
-		{
-			eval('tags[i].style.'+attrib+'="'+value+'";');
-		} 
-}
-
-function sidebarConfig() {
-	for (i in G_prefs) {
-		G_prefs[i] = getPref(i);
-	}
-	historyBox();
-	// update backgrounds
-		
-	//checkbackground();
-
-}
-
-function changecss(myclass,element,value) {
-	var CSSRules
-	if (document.all) {
-		CSSRules = 'rules'
-	}
-	else if (document.getElementById) {
-		CSSRules = 'cssRules'
-	}
-	for (var i = 0; i < document.styleSheets[0][CSSRules].length; i++) {
-		if (document.styleSheets[0][CSSRules][i].selectorText == myclass) {
-			document.styleSheets[0][CSSRules][i].style[element] = value
+var DPRConfig = {
+	sidebarConfig:function() {
+		for (i in G_prefs) {
+			G_prefs[i] = getPref(i);
 		}
-	}	
-}
-
-function checkbackground() {
-	var wbk = G_prefs['bktype'];
-	
-	if(/col/.exec(wbk)) {
-		document.styleSheets[0]['cssRules'][7].style.backgroundColor = G_prefs['colbk'];  // paperback
-		document.body.style.backgroundColor = G_prefs['colbk'];
-	}
-	else {
-		document.styleSheets[0]['cssRules'][7].style.backgroundColor = '';  // paperback
-		document.body.style.backgroundColor = '';
-	}
-	if(/img/.exec(wbk)) {
-		document.styleSheets[0]['cssRules'][7].style.backgroundImage = G_prefs['imgbk'];  // paperback
-		document.body.style.backgroundImage = G_prefs['imgbk'];
-	}
-	else {
-		document.styleSheets[0]['cssRules'][7].style.backgroundImage = '';  // paperback
-		document.body.style.backgroundImage = '';
-	}
-
-	var sbk = G_prefs['bkcptype'];
-
-	if(/col/.exec(sbk)) {
-		document.styleSheets[0]['cssRules'][8].style.backgroundColor = G_prefs['colbkcp'];  // chromeback
-	}
-	else {
-		document.styleSheets[0]['cssRules'][8].style.backgroundColor = '';  // chromeback
-	}
-	if(/img/.exec(sbk)) {
-		document.styleSheets[0]['cssRules'][8].style.backgroundImage = G_prefs['imgbkcp'];  // chromeback
-	}
-	else {
-		document.styleSheets[0]['cssRules'][8].style.backgroundImage = '';  // chromeback
-	}
+		DPRNav.historyBox();
+		document.styleSheets[0]['cssRules'][0].style.color = G_prefs['coltext']; 
+		document.styleSheets[0]['cssRules'][0].style.fontFamily = G_prefs['colfont']; 
+		document.styleSheets[0]['cssRules'][1].style.fontSize = G_prefs['colsize'] + 'px'; 
+		
+		document.styleSheets[0]['cssRules'][2].style.fontSize = Math.round(parseInt(G_prefs['colsize'])*.9) + 'px';  // select, etc.
+		document.styleSheets[0]['cssRules'][2].style.backgroundColor = G_prefs['colInput'];  // select, etc.
+		
+		document.styleSheets[0]['cssRules'][3].style.fontSize = Math.round(parseInt(G_prefs['colsize'])*.8) + 'px';  // small
+		document.styleSheets[0]['cssRules'][4].style.fontSize = Math.round(parseInt(G_prefs['colsize'])*.7) + 'px';  // tiny
+		document.styleSheets[0]['cssRules'][5].style.fontSize = Math.round(parseInt(G_prefs['colsize'])*1.25) + 'px';  // large
+		document.styleSheets[0]['cssRules'][6].style.fontSize = Math.round(parseInt(G_prefs['colsize'])*1.5) + 'px';  // huge
+		
+		document.styleSheets[0]['cssRules'][9].style.backgroundColor = G_prefs['colButton'];  // buttons
+		document.styleSheets[0]['cssRules'][10].style.backgroundImage = '-moz-radial-gradient('+G_prefs['colButtonSel']+','+G_prefs['colbkcp']+')';  // buttons
+		document.styleSheets[0]['cssRules'][11].style.backgroundImage = '-moz-radial-gradient('+G_prefs['colButtonSel']+','+G_prefs['colbkcp']+')';  // buttons
+	},
 }
 
