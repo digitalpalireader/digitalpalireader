811b7a4fd29f1cb12b934334d984548ce783961b
yuttadhammo <yuttadhammo@gmail.com>
2012-01-23 08:43:04 +0530

    dbv

--- digitalpalireader/content/js/bv.js
+++ digitalpalireader/content/js/bv.js
@@ -1,6 +1,6 @@
 function bv(rnd,static) {
 	var xmlhttp = new window.XMLHttpRequest();
-	xmlhttp.open("GET", 'chrome://digitalpalireader/content/dbv.html', false);
+	xmlhttp.open("GET", 'chrome://digitalpalireader/content/etc/dbv.html', false);
 	xmlhttp.send(null);
 	var xmlDoc = xmlhttp.responseXML.documentElement;
 	var divs = xmlDoc.getElementsByTagName('div');
@@ -16,6 +16,20 @@ function bv(rnd,static) {
 		var theDay = Math.round(((today - first) / 1000 / 60 / 60 / 24) + .5, 0);
 		var no = theDay-1;
 	}
-	var verse = divs[no].textContent;
-	return [verse];
+
+	var verse = divs[no];
+	var head = verse.getElementsByTagName('h2')[0].textContent;
+	var text = [];
+	var cite = '';
+
+	var ps = verse.getElementsByTagName('p');
+	for(i in ps) {
+		if (ps[i].className == 'text') {
+			text.push(ps[i].textContent);
+		}
+		else if (ps[i].className == 'citation') {
+			cite = ps[i].textContent;
+		}
+	}
+	return [head,text,cite];
 }
