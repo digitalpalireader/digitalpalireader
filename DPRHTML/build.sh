88f3c10dc50b2e07339af4e995b73be07c3fe041
Partho P. Das <parthopdas@live.com>
2020-01-14 15:58:46 +1000

    Fixing CI

--- DPRHTML/build.sh
+++ DPRHTML/build.sh
@@ -2,12 +2,20 @@ APP_NAME=dprweb
 OUTDIR="${1:-../../build}"
 SOURCEDIR="${2:-$OUTDIR/..}"
 
+echo "Building dprweb extensions"
+pushd DPRHTML/extensions
+yarn install
+yarn lint
+yarn build
+yarn citest
+popd
+
 rm -f $OUTDIR/$APP_NAME.zip
 
 # generate the ZIP file
 echo "Generating $APP_NAME.zip... ($SOURCEDIR -> $OUTDIR)"
 pushd $SOURCEDIR
-zip -r $OUTDIR/$APP_NAME.zip * -x \*.git\* *.zip
+zip -r $OUTDIR/$APP_NAME.zip * -x \*.git\* -x \*node_modules\* *.zip
 popd
 
 if hash notify-send 2>/dev/null; then
