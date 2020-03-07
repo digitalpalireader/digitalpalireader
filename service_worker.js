9cbaf9068c682a9dcd38e1dc9dd1cbcf88d3878c
dangeol <do.dev@outlook.com>
2020-03-07 22:43:24 +0100

    feat(pwa) service_worker and beforeinstallprompt

---
+++ service_worker.js
@@ -0,0 +1,52 @@
+var CACHE_NAME = 'dpr-cache';
+var urlsToCache = [
+  '/',
+  '/digitalpalireader',
+  '/DPRHTML',
+  '/DPRMyanmar',
+  '/DPRThai',
+  '/DPRWeb',
+  '/sanskrit',
+  '/sidebar'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['dpr-cache-1', 'dpr-cache-2'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
