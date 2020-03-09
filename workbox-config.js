542458fdeb99172c47c190c1f7682babc4424b0d
dangeol <do.dev@outlook.com>
2020-03-09 20:07:30 +0100

    feat(pwa) workbox injectManifest

--- workbox-config.js
+++ workbox-config.js
@@ -1,27 +1,13 @@
-//run command: 'workbox generateSW'
+//run command: 'workbox injectManifest'
 module.exports = {
-  cacheId: "dpr-1",
   globDirectory: "./",
   globPatterns: [
-    "**/*.{css,js,xml,png,eot,otf,svg,ttf,woff,woff2}"
+    "**/*.{xml,png,eot,otf,svg,ttf,woff,woff2}"
   ],
   globIgnores: [
     "**/service_worker.js"
   ],
-  swDest: "workbox_sw/service_worker.js",
-  skipWaiting: true,
-  clientsClaim: true,
-  maximumFileSizeToCacheInBytes: 12000000,
-  runtimeCaching: [
-    {
-      urlPattern: /\.(?:html|htm|xml)$/,
-      handler: "StaleWhileRevalidate",
-      options: {
-        cacheName: "markup",
-        expiration: {
-          maxAgeSeconds: 60 * 60 * 24 * 7,
-        },
-      },
-    }
-  ],
+  swSrc: "service_worker_src.js",
+  swDest: "service_worker.js",
+  maximumFileSizeToCacheInBytes: 12000000
 };
