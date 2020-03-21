d2567e9434952aa0495b0395ffe131bfa2f26e60
vasil <sumbodhi@gmail.com>
2020-03-21 11:20:46 +0200

    Daily Buddha Vacana on Home page #128

--- DPRHTML/index.js
+++ DPRHTML/index.js
@@ -34,6 +34,7 @@ function mainInitialize() {
           () => {
             __dprViewModel.showLandingFeature();
             initFeedbackFormParameters();
+            showBv();
           });
     return;
   }
