919b3ea376017ad3f118f0c71d42c2d01812656c
yuttadhammo <yuttadhammo@gmail.com>
2020-04-22 01:07:03 -0400

    added uddhaṅgama in compounds

--- digitalpalireader/content/js/irreg.js
+++ digitalpalireader/content/js/irreg.js
@@ -1521,6 +1521,7 @@ G_irregNoun['nti'] = 'iti';
 // compounds
 
 G_irregNoun['aha"n`'] = 'aha.m';
+G_irregNoun['uddha"n`'] = 'uddha.m';
 G_irregNoun['idam`'] = 'ida';
 G_irregNoun['etad`'] = 'eta';
 G_irregNoun['khv`'] = 'kho';
