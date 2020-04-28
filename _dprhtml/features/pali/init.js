e10740ea2a6675c1e75b22f738409991adab821a
smkvr98 <smkvr98@gmail.com>
2020-04-28 23:18:03 +0530

    init Pali feature with resources

--- _dprhtml/features/pali/init.js
+++ _dprhtml/features/pali/init.js
@@ -1,10 +1,22 @@
-'use strict';
+"use strict";
 
 const initializePaliFeature = () => {
-  console.log("Pali");
-};
-
+  const url = new URL(window.location);
+  const subfeat = url.searchParams.get("subfeat");
+  console.log(subfeat);
+  let resourceLinks = {
+    PCRS: "../digitalpalireader/content/etc/grammar/compound.htm",
+    PTCS: "features/pali/grammar/cheat1.html",
+    PGPL: '../digitalpalireader/content/etc/grammar/GMD/index.html',
+    abbrvPED: "../digitalpalireader/content/etc/grammar/ped-abbv.htm",
+    abbrvCPED: "../digitalpalireader/content/etc/grammar/cped-abbv.htm",
+    //quiz: "./features/pali/quiz/quiz.html",
+  };
+  let link = resourceLinks[subfeat];
+  console.log(link);
+  link ? $("#paliContent").attr('src',link) : openNewSidebar();
+  };
 
 const initializePaliSidebarTab = () => {
   console.log("Sidebar Initialized");
-}
+};
