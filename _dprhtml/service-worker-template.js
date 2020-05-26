ef4f09a23aeec44962b21a5b324abb1fc58fd584
Partho P. Das <parthopdas@live.com>
2020-05-26 20:03:22 +1000

    WIP - 2

--- _dprhtml/service-worker-template.js
+++ _dprhtml/service-worker-template.js
@@ -1,8 +1,64 @@
+/*
+ * Version: #{DeploymentReleaseNumber}#
+ */
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-workbox.precaching.precacheAndRoute(
-  self.__WB_MANIFEST,
-  {
-    ignoreURLParametersMatching: [/.*/],
+workbox.core.setCacheNameDetails({
+  prefix: 'dpr',
+  suffix: 'v5',
+  precache: 'precache',
+  runtime: 'runtime',
+  googleAnalytics: 'google-analytics-name',
+});
+
+addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    workbox.core.skipWaiting();
   }
+});
+
+// workbox.precaching.precacheAndRoute(
+//   self.__WB_MANIFEST,
+//   {
+//     ignoreURLParametersMatching: [/.*/],
+//   }
+// );
+
+
+const matchFunction = x => {
+  console.warn('>>>> matchFunction', x);
+}
+
+const handler = x => {
+  console.warn('>>>> handler', x);
+}
+
+workbox.routing.registerRoute(
+  matchFunction,
+  handler,
 );
+
+// workbox.routing.registerRoute(
+//   ({url}) => url.origin === self.location.origin && /^\/_dprhtml\//i.test(url.pathname),
+//   new workbox.strategies.CacheFirst({
+//     cacheName: 'tipitaka-my',
+//     plugins: [
+//       new workbox.expiration.ExpirationPlugin({
+//         maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year.
+//       }),
+//     ],
+//   }),
+// );
+
+// workbox.routing.registerRoute(
+//   ({url}) => url.origin === self.location.origin && /^\/tipitaka\/my\//i.test(url.pathname),
+//   new workbox.strategies.CacheFirst({
+//     cacheName: 'tipitaka-my-2',
+//     plugins: [
+//       new workbox.expiration.ExpirationPlugin({
+//         maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year.
+//       }),
+//     ],
+//   }),
+// );
--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,12 @@
 module.exports = {
   "maximumFileSizeToCacheInBytes": 5000000,
+  "dontCacheBustURLsMatching": /\.\w{32}\./,
   "globDirectory": "_dprhtml/",
   "globPatterns": [
     "**/*.*"
+  ],
+  "globIgnores": [
+    "**/*.{map,sh}",
   ],
   "swDest": "_dprhtml/service-worker.js",
   "swSrc": "_dprhtml/service-worker-template.js",
