fb60f8ce8d7a7c8d4177aed26b81f6515661acff
Partho P. Das <parthopdas@live.com>
2020-05-27 17:54:40 +1000

    #96 - fixes
    > os agnostic path
    > relative path
    > direct association of name and declLine1 from source
    > saving output of callsAndDecls and funcInfo to test updates to script

--- tools/migration/mapper.py
+++ tools/migration/mapper.py
@@ -2,22 +2,35 @@ import json
 import os
 import os.path
 import re
-z = {}
+from pathlib import Path
+import functools
+
+"""
+TODO
+1. Fix up cnd0 vs cnd1
+2. Sort console output
+3. Sort Called (e.g "conjugate")
+4. What is leftFunc.pop
+5. Name better
+6. Use descriptive function/variable names instead of comments
+7. Eye on continued correctness
+
+Optional:
+1. Write 'pure' 'complete' functions
+2. Think deterministic code
+   1. 0 global variables
+   2. Think data transformations instead of imperative code. i.e. Use map/reduce/filter rather than for/while and other imperative constructs
+
+"""
+
 listOfmatches = {}
 funcToFile = {}
 funcXfileDeclr = {}
 expByFile = {}
 
-with open("x.json") as x:
-  presentDict = json.load(x)
-funcList = presentDict["functions"]
-declList = presentDict["declrLine1"]
+leftFunc = functools.reduce(lambda acc,e : acc.update({e["name"]: e["declLine1"]}) or acc, json.load(open("funcInfo.json")), {})
 
-while len(funcList):
-    z[funcList.pop()] = declList.pop()
-leftFunc = dict(z)
-
-for i in z:
+for i in leftFunc:
     funcToFile[i] = {}
 
 
@@ -32,7 +45,7 @@ def searchDeclr(line, fileName, lnNo):
 
 
 def searchCall(line, fileName, lnNo):
-    for i in z:
+    for i in leftFunc:
         y = re.match(r'.*\b'+re.escape(i)+r'\b', line)
         if(y):
             print(i+" called at line:"+str(lnNo))
@@ -45,8 +58,8 @@ funcXFile = {}
 
 
 def findDeclrandCalls():
-    for dirpath, dirnames, filenames in os.walk("../_dprhtml"):
-        if('external' not in dirpath.split('/')):
+    for dirpath, dirnames, filenames in os.walk(Path("../../_dprhtml")):
+        if('external' not in dirpath.split(os.path.sep)):
             for filename in [f for f in filenames if f.split(".")[-1] in ["html", "js"]]:
                 fileName = (os.path.join(dirpath, filename))
                 findInfile(fileName)
@@ -55,7 +68,7 @@ def findDeclrandCalls():
 def findInfile(fileName):
     print(fileName)
     print("__________________\n")
-    for i, line in enumerate(open(fileName), 1):
+    for i, line in enumerate(open(fileName, encoding="utf8"), 1):
         searchDeclr(line, fileName, i)  # step 2
         searchCall(line, fileName, i)  # step 3
     print("__________________\n")
@@ -67,19 +80,19 @@ findDeclrandCalls()
 
 listOfFunc2Exp = [a for a in funcToFile if len(funcToFile[a]) > 1]
 
-DeclareVars = {a: b for (a, b) in funcXfileDeclr.items()
-               if a in listOfFunc2Exp}  # DeclareSites
-ChangeVars = {a: b for (a, b) in funcToFile.items()
-              if a in listOfFunc2Exp}  # CallSites
+DeclareSites = {a: b for (a, b) in funcXfileDeclr.items()
+               if a in listOfFunc2Exp}
+CallSites = {a: b for (a, b) in funcToFile.items()
+              if a in listOfFunc2Exp}
 
-for i in ChangeVars:  # step5
-    ChangeVars[i].pop(DeclareVars[i].split(":")[0], None)
+for i in CallSites:  # step5
+    CallSites[i].pop(DeclareSites[i].split(":")[0], None)
 
 finFile = {}
-for i in ChangeVars:
+for i in CallSites:
     finFile[i] = {}
-    finFile[i]["Declared"] = DeclareVars[i]
-    finFile[i]["Called"] = ChangeVars[i]
+    finFile[i]["Declared"] = DeclareSites[i]
+    finFile[i]["Called"] = CallSites[i]
 
-with open("calls&Declr.json", "w") as f:
+with open("callsAndDecls.json", "w", encoding="utf8") as f:
     json.dump(finFile, f)
