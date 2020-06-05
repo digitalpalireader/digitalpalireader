2904678a3c2a8dee36777c2953a49f443a705324
smkvr98 <smkvr98@gmail.com>
2020-06-05 17:30:42 +0530

    Format mapper.py

--- tools/migration/mapper.py
+++ tools/migration/mapper.py
@@ -5,30 +5,13 @@ import re
 from pathlib import Path
 import functools
 
-"""
-TODO
-1. Fix up cnd0 vs cnd1
-2. Sort console output
-3. Sort Called (e.g "conjugate")
-4. What is leftFunc.pop
-5. Name better
-6. Use descriptive function/variable names instead of comments
-7. Eye on continued correctness
-
-Optional:
-1. Write 'pure' 'complete' functions
-2. Think deterministic code
-   1. 0 global variables
-   2. Think data transformations instead of imperative code. i.e. Use map/reduce/filter rather than for/while and other imperative constructs
-
-"""
-
 listOfmatches = {}
 funcToFile = {}
 funcXfileDeclr = {}
 expByFile = {}
 
-leftFunc = functools.reduce(lambda acc,e : acc.update({e["name"]: e["declLine1"]}) or acc, json.load(open("funcInfo.json")), {})
+leftFunc = functools.reduce(lambda acc, e: acc.update(
+    {e["name"]: e["declLine1"]}) or acc, json.load(open("funcInfo.json")), {})
 allFunc = dict(leftFunc)
 
 for i in leftFunc:
@@ -53,6 +36,7 @@ def searchCall(line, fileName, lnNo):
             if(funcToFile[i].get(fileName) is None):
                 funcToFile[i][fileName] = []
             funcToFile[i][fileName].append((lnNo))
+
 
 listOfFunc2Exp = {}
 funcXFile = {}
@@ -82,42 +66,43 @@ findDeclrandCalls()
 listOfFunc2Exp = [a for a in funcToFile if len(funcToFile[a]) > 1]
 
 DeclareSites = {a: b for (a, b) in funcXfileDeclr.items()
-               if a in listOfFunc2Exp}
+                if a in listOfFunc2Exp}
 CallSites = {a: b for (a, b) in funcToFile.items()
-              if a in listOfFunc2Exp}
+             if a in listOfFunc2Exp}
 for i in CallSites:  # step5
-    fN,lN=DeclareSites[i].split(":")
+    fN, lN = DeclareSites[i].split(":")
     CallSites[i][fN].remove(int(lN))
 
 finDict = {}
+
 for i in CallSites:
     finDict[i] = {}
     finDict[i]["Declared"] = DeclareSites[i]
     finDict[i]["Called"] = CallSites[i]
 
-finFile = sorted(finDict.items(),key=lambda a:len(a[1]["Called"]) )
+finFile = sorted(finDict.items(), key=lambda a: len(a[1]["Called"]))
 
 with open("callsAndDecls.json", "w", encoding="utf8") as f:
     json.dump(finFile, f)
 
-func2Exp4File = {finDict[v]["Declared"].split(":")[0]:[] for v in finDict}
+func2Exp4File = {finDict[v]["Declared"].split(":")[0]: [] for v in finDict}
 
 for i in finDict:
-        [fileN,lnNo]=finDict[i]["Declared"].split(":")
-        func2Exp4File[fileN].append({'name':i,'Line':lnNo,'Called@':finDict[i]["Called"]})
+    [fileN, lnNo] = finDict[i]["Declared"].split(":")
+    func2Exp4File[fileN].append(
+        {'name': i, 'Line': lnNo, 'Called@': finDict[i]["Called"]})
 
-numOfChangesForFile={}
+numOfChangesForFile = {}
 
 for i in func2Exp4File:
-    x=0
+    x = 0
     for j in func2Exp4File[i]:
         for k in j["Called@"]:
-                x+=(len(j["Called@"][k]) )
-    numOfChangesForFile[i]=x
+            x += (len(j["Called@"][k]))
+    numOfChangesForFile[i] = x
 
-func2Exp4FileSorted = sorted(func2Exp4File.items(),key=lambda a:numOfChangesForFile[a[0]] )
+func2Exp4FileSorted = sorted(
+    func2Exp4File.items(), key=lambda a: numOfChangesForFile[a[0]])
 
 with open("file2Exp4File.json", "w", encoding="utf8") as f:
     json.dump(func2Exp4FileSorted, f)
-
-
