29001700b053316f693f022aaca8d599d8b93f56
smkvr98 <smkvr98@gmail.com>
2020-06-16 03:26:12 +0530

    increase readability

--- tools/migration/modifier.py
+++ tools/migration/modifier.py
@@ -1,28 +1,42 @@
 import json
 import re
-bg = 1
-e = 2
-file2Exp4file = json.load(open("file2Exp4File.json"))
-for DeclfileObj in file2Exp4file[bg:e]:
-    print(DeclfileObj[0])
-    fModName = "DPR_"+DeclfileObj[0].split("/")[-1].split(".")[0]+"_mod"
-    f = (open(DeclfileObj[0], 'r+'))
-    ftext = f.readlines()
-    ftext.append("\nvar "+fModName+" = ( function () {\nreturn {\n")
-    [ftext.append("{0} : {0}".format(x["name"])) for x in DeclfileObj[1]]
-    ftext.append("\n}\n})()\n")
-    f.seek(0)
-    f.writelines(ftext)
-    f.close
-    for funcs in (DeclfileObj[1]):
-        print("\nfunction - ", funcs['name'], "\n")
-        for callFile, lnNo in (funcs['Called@'].items()):
-            f = (open(callFile, 'r+'))
-            flines = (f.readlines())
-            for line in lnNo:
-                print(flines[line-1], "\n changed to:\n")
-                flines[line-1] = (re.sub(re.escape(funcs['name']),
-                                         fModName+"."+funcs['name'], flines[line-1]))
-                print(flines[line-1], "\n")
-            f.seek(0)
-            f.writelines(flines)
+
+
+def handleFileOfDeclaration(element4File2Exp, moduleName):
+    fileOfDeclaration = (open(element4File2Exp[0], 'r+'))
+    fileOfDeclarationtext = fileOfDeclaration.readlines()
+    fileOfDeclarationtext.append(
+        "\nvar "+moduleName+" = ( function () {\nreturn {\n")
+    [fileOfDeclarationtext.append("{0} : {0}".format(
+        element4Func2Exp["name"])) for element4Func2Exp in element4File2Exp[1]]
+    fileOfDeclarationtext.append("\n}\n})()\n")
+    fileOfDeclaration.seek(0)
+    fileOfDeclaration.writelines(fileOfDeclaration)
+    fileOfDeclaration.close
+
+def handleCallSites4Func(element4Func2Exp):
+    for callFile, lineNos in (element4Func2Exp['Called@'].items()):
+        fileOfCallSite = (open(callFile, 'r+'))
+        fileOfCallSiteText = (fileOfCallSite.readlines())
+        for line in lineNos:
+            print(fileOfCallSiteText[line-1], "\n changed to:\n")
+            fileOfCallSiteText[line-1] = (re.sub(re.escape(element4Func2Exp['name']),
+                                                 moduleName+"."+element4Func2Exp['name'], fileOfCallSiteText[line-1]))
+            print(fileOfCallSiteText[line-1], "\n")
+        fileOfCallSite.seek(0)
+        fileOfCallSite.writelines(fileOfCallSiteText)
+
+def genModuleName(fileName):
+    return "DPR_"+fileName.split("/")[-1].split(".")[0]+"_mod"
+
+def exposeFiles(dataFileName,beginningIndex,endIndex)
+  funcsToExpByfile = json.load(open(dataFileName,"r",encoding="utf8"))
+  for element4File2Exp in funcsToExpByfile[beginningIndex:endIndex]:
+      print(element4File2Exp[0])
+      moduleName = genModuleName(element4File2Exp[0])
+      handleFileOfDeclaration(element4File2Exp, moduleName)
+      for element4Func2Exp in (element4File2Exp[1]):
+          print("\nfunction - ", element4Func2Exp['name'], "\n")
+          handleCallSites4Func(element4Func2Exp)
+
+exposeFiles("functionsToExposeForFile.json",beginningIndex=0,endIndex=2)
