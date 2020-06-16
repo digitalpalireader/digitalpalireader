f1597e423b2a82850c8b278662b8895af98dac82
smkvr98 <smkvr98@gmail.com>
2020-06-16 11:10:57 +0530

    fix modifier.py

--- tools/migration/modifier.py
+++ tools/migration/modifier.py
@@ -1,20 +1,20 @@
 import json
 import re
 
-
 def handleFileOfDeclaration(element4File2Exp, moduleName):
     fileOfDeclaration = (open(element4File2Exp[0], 'r+'))
-    fileOfDeclarationtext = fileOfDeclaration.readlines()
-    fileOfDeclarationtext.append(
+    fileOfDeclarationText = fileOfDeclaration.readlines()
+    fileOfDeclarationText.append(
         "\nvar "+moduleName+" = ( function () {\nreturn {\n")
-    [fileOfDeclarationtext.append("{0} : {0}".format(
+    [fileOfDeclarationText.append("{0} : {0}".format(
         element4Func2Exp["name"])) for element4Func2Exp in element4File2Exp[1]]
-    fileOfDeclarationtext.append("\n}\n})()\n")
+    fileOfDeclarationText.append("\n}\n})()\n")
+    print(fileOfDeclarationText)
     fileOfDeclaration.seek(0)
-    fileOfDeclaration.writelines(fileOfDeclaration)
-    fileOfDeclaration.close
+    fileOfDeclaration.writelines(fileOfDeclarationText)
+    fileOfDeclaration.close()
 
-def handleCallSites4Func(element4Func2Exp):
+def handleCallSites4Func(element4Func2Exp, moduleName):
     for callFile, lineNos in (element4Func2Exp['Called@'].items()):
         fileOfCallSite = (open(callFile, 'r+'))
         fileOfCallSiteText = (fileOfCallSite.readlines())
@@ -25,11 +25,12 @@ def handleCallSites4Func(element4Func2Exp):
             print(fileOfCallSiteText[line-1], "\n")
         fileOfCallSite.seek(0)
         fileOfCallSite.writelines(fileOfCallSiteText)
+        fileOfCallSite.close()
 
 def genModuleName(fileName):
     return "DPR_"+fileName.split("/")[-1].split(".")[0]+"_mod"
 
-def exposeFiles(dataFileName,beginningIndex,endIndex)
+def exposeFiles(dataFileName,beginningIndex,endIndex):
   funcsToExpByfile = json.load(open(dataFileName,"r",encoding="utf8"))
   for element4File2Exp in funcsToExpByfile[beginningIndex:endIndex]:
       print(element4File2Exp[0])
@@ -37,6 +38,6 @@ def exposeFiles(dataFileName,beginningIndex,endIndex)
       handleFileOfDeclaration(element4File2Exp, moduleName)
       for element4Func2Exp in (element4File2Exp[1]):
           print("\nfunction - ", element4Func2Exp['name'], "\n")
-          handleCallSites4Func(element4Func2Exp)
+          handleCallSites4Func(element4Func2Exp, moduleName)
 
 exposeFiles("functionsToExposeForFile.json",beginningIndex=0,endIndex=2)
