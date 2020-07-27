dfda9e51991e8541a6938ece54b4dfd7224fbe4a
dangeol <do.dev@outlook.com>
2020-07-27 09:51:53 +0200

    feat(pwa) translations ati, en, sa

--- _dprhtml/features/installation/init.js
+++ _dprhtml/features/installation/init.js
@@ -7,6 +7,68 @@ var DPRComponentRegistry = (function () {
 
   const registry = [
     {
+      id: 'my',
+      name: 'Myanmar tipitaka (with commentary and sub-commentary)',
+      shortDescription: '',
+      routeRegExp: ({url}) => url.origin === self.location.origin && /^\/tipitaka\/my\//i.test(url.pathname),
+      isAvailable: () => true,
+      type: componentTypeTipitaka,
+      sizeMB: 113,
+      getFileList: async () => {
+        // TODO: Generate this list from during the build process.
+        const myFiles = ['a10a','a10m','a10t','a11a','a11m','a11t','a1a','a1m','a1t','a2a','a2m','a2t','a3a','a3m','a3t','a4a','a4m','a4t','a5a','a5m','a5t','a6a','a6m','a6t','a7a','a7m','a7t','a8a','a8m','a8t','a9a','a9m','a9t','b1m','b2m','d1a','d1m','d1t','d2a','d2m','d2t','d3a','d3m','d3t','g1m','g2m','g3m','g4m','g5m','k10a','k10m','k11m','k12a','k12m','k13a','k13m','k14a','k14m','k15a','k15m','k16m','k17m','k18m','k19m','k1a','k1m','k20m','k21m','k2a','k2m','k3a','k3m','k4a','k4m','k5a','k5m','k6a','k6m','k7a','k7m','k8a','k8m','k9a','k9m','m1a','m1m','m1t','m2a','m2m','m2t','m3a','m3m','m3t','n1m','n2m','n3m','n4m','n5m','n6m','n7m','n8m','n9m','s1a','s1m','s1t','s2a','s2m','s2t','s3a','s3m','s3t','s4a','s4m','s4t','s5a','s5m','s5t','v10t','v11t','v12t','v13t','v14t','v15t','v16t','v17t','v18t','v1a','v1m','v1t','v2a','v2m','v2t','v3a','v3m','v3t','v4a','v4m','v4t','v5a','v5m','v5t','v6a','v6m','v6t','v7t','v8t','v9t','x1a','x1m','x2a','x2m','y10m','y11m','y12m','y13m','y14m','y1a','y1m','y1t','y2a','y2m','y2t','y3a','y3m','y3t','y4a','y4m','y4t','y5a','y5m','y5t','y6a','y6m','y6t','y7m','y8m','y9a','y9m','y9t',]
+        return myFiles.map(f => `/tipitaka/my/${f}.xml`)
+      },
+    },
+    {
+      id: 'th',
+      name: 'Thai tipitaka (with commentary and sub-commentary)',
+      shortDescription: '',
+      routeRegExp: ({url}) => url.origin === self.location.origin && /^\/tipitaka\/th\//i.test(url.pathname),
+      isAvailable: () => true,
+      type: componentTypeTipitaka,
+      sizeMB: 21,
+      getFileList: async () => {
+        // TODO: Generate this list from during the build process.
+        const thFiles = ['a10m','a11m','a1m','a2m','a3m','a4m','a5m','a6m','a7m','a8m','a9m','d1m','d2m','d3m','k1m','k2m','k3m','k4m','k5m','m1m','m2m','m3m','s1m','s2m','s3m','s4m','s5m','v1m','v2m','v3m','v4m','v5m','v6m',]
+        return thFiles.map(f => `/tipitaka/th/${f}.xml`)
+      },
+    },
+    {
+      id: 'en',
+      name: 'English language (dictionaries PED, CPED, DPPN)',
+      shortDescription: '',
+      routeRegExp: ({url}) => url.origin === self.location.origin && /^\/en\//i.test(url.pathname),
+      isAvailable: () => true,
+      type: componentTypeLanguage,
+      sizeMB: 19,
+      getFileList: async () => {
+        const pedArr = Array.from({ length: 5 }, (_, k)=> k).map(n => `/en/ped/${n}/ped.xml`)
+        const cepdArr = '/en/cepd/index.js'
+        const cpedArr = '/en/cped/index.js'
+        const dppnArr = Array.from({ length: 10 }, (_, k)=> k+1).map(n => `/en/dppn/${n}.xml`).concat('/en/dppn/abbrev.xml')
+        return [].concat(...[pedArr, cepdArr, cpedArr, dppnArr])
+      },
+    },
+    {
+      id: 'sa',
+      name: 'Sanskrit language (dictionary and roots)',
+      shortDescription: '',
+      routeRegExp: ({url}) => url.origin === self.location.origin && /^\/sa\//i.test(url.pathname),
+      isAvailable: () => true,
+      type: componentTypeLanguage,
+      sizeMB: 94,
+      getFileList: async () => {
+        await DPR_PAL.addOneJS('/components/sa/sa_dict_list.js')
+        await DPR_PAL.addOneJS('/components/sa/sa_roots_list.js')
+        const saDictPath = '/components/sa/dict'
+        const saRootsPath = '/components/sa/roots'
+        const saDictFiles = DPR_G.saDictFiles.map(x => `${saDictPath}/${x}`)
+        const saRootsFiles = DPR_G.saRootsFiles.map(x => `${saRootsPath}/${x}`)
+        return [].concat(...[saDictFiles, saRootsFiles])
+      },
+    },
+    {
       id: 'bt',
       name: 'Buddhist Texts',
       shortDescription: '',
---
+++ components/ati/translations_list.js
@@ -0,0 +1,5 @@
+'use strict';
+
+DPR_G.atiAssetsToPrefetch = [
+  
+];
