a35eea1c106a290914612938c316ef9f3b59bc13
Daniel Oellers <44405794+dangeol@users.noreply.github.com>
2021-01-05 23:08:25 +0100

    fix(sidebar) dpr not loading quick workaround (#308)
    * fix(sidebar) dpr not loading quick workaround
    
    * fix(sidebar) dpr not loading, workaround, part 2

--- _dprhtml/js/web/xml_sidebar.js
+++ _dprhtml/js/web/xml_sidebar.js
@@ -88,6 +88,12 @@ var DPRXML = {
 
     var xmlDoc = await XML_Load.loadXMLFileAsync(nikbookhier, 0);
 
+    if (xmlDoc === null){
+      console.error('Error loading file', e)
+      DPR_prefload_mod.saveSearchSettings(DPR_G.DPR_prefsinfo[DPR_prefload_mod.searchSettingsKeyName].defaultValue);
+      return;
+    }
+
     var nik = nikaya;
 
     var meta = (depth > 0 ? $('#tsoPmeta').prop('selectedIndex') : 0);
