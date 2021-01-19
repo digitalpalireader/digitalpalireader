8181dfd64821d0c692e50351881799b05f440af4
Catalin Iosif <ciosif@axway.com>
2021-01-19 11:52:01 +0200

    #266 - Swipe gestures - separate per panel

--- _dprhtml/js/send.js
+++ _dprhtml/js/send.js
@@ -6,7 +6,7 @@ function eventSend(event,internal) {
   if(!event) return;
   if(event.ctrlKey || event.metaKey || event.which == 2) return true;
   if(event.shiftKey) return 'shift';
-  if((event.which == 1 || event.charCode || event.gesture) && internal) return 'internal';
+  if((event.which == 1 || event.charCode || event.dpr_gesture) && internal) return 'internal';
   if (event.which == 1) return false;
   return 'right';
 }
