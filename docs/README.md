6e67d12b2378d6157bac12fa46297e8a884d421c
Roman Rogowski <rrogowski@aurorasolar.com>
2021-07-22 21:41:59 -0400

    chore: add english help documentation from palemoon

---
+++ docs/README.md
@@ -0,0 +1,13 @@
+# Sphinx Documentation Generation
+
+## Install Dependencies
+
+    pip install --requirement requirements.txt
+
+## Generate English Documentation
+
+    make html
+
+## Manage Translations
+
+See https://docs.readthedocs.io/en/stable/guides/manage-translations.html.
---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+# Configuration file for the Sphinx documentation builder.
+# See: https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+author = 'Sirimangalo International'
+copyright = '2021, Sirimangalo International'
+project = 'Digital Pāli Reader'
+release = '0.0.0'
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+extensions = []
+gettext_compact = False
+gettext_uuid = True
+html_static_path = []
+html_theme = 'alabaster'
+templates_path = []
